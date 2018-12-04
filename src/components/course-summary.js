3219f6fba41fd476e5fd1f933af0178f25a6a953
AdmiralCoco <dawingc@hotmail.com>
2018-12-04 16:22:53 -0500

    Clean up CSS
    - Remove d2l-typography (imported at top level)
    - Added prefix to classes
    - Alphabetize rules

--- src/components/course-summary.js
+++ src/components/course-summary.js
@@ -9,97 +9,94 @@ import 'd2l-link/d2l-link.js';
 class CourseSummary extends LocalizeMixin(RouteLocationsMixin(PolymerElement)) {
 	static get template() {
 		return html `
-			<style include="d2l-typography"></style>
 			<style>
 				:host {
 					display: inline;
 				}
 
-				.container {
+				.discovery-course-summary-container {
 					display: flex;
 					flex-direction: column;
 					overflow: hidden;
 					padding: 1rem;
 				}
 
-				.breadcrumbs {
+				.discovery-course-summary-breadcrumbs {
+					align-items: center;
 					display: flex;
 					flex-direction: row;
-					align-items: center;
 				}
 
-				.breadcrumbs>* {
+				.discovery-course-summary-breadcrumbs>* {
 					margin: 2px;
 				}
 
-				.title {
+				.discovery-course-summary-title {
 					margin-top: 0.5rem;
 				}
 
-				.d2l-heading-1 {
+				.discovery-course-summary-d2l-heading-1 {
 					margin-top: 0 !important;
 				}
 
-				.d2l-heading-4 {
-					margin-top: 0 !important;
+				.discovery-course-summary-d2l-heading-4 {
 					margin-bottom: 1rem !important;
+					margin-top: 0 !important;
 				}
 
-				.keyOutcomes {
+				.discovery-course-summary-key-outcomes {
 					margin-bottom: 1rem;
 				}
 
-				.keyOutcomes ul{
-					list-style-type: disc;
-					-webkit-columns: 2;
+				.discovery-course-summary-key-outcomes ul{
+					-moz-column-gap: 3rem;
 					-moz-columns: 2;
+					-webkit-column-gap: 3rem;
+					-webkit-columns: 2;
+
+					column-gap: 3rem;
 					columns: 2;
 					list-style-position: inside;
-					-webkit-column-gap: 3rem;
-					-moz-column-gap: 3rem;
-					column-gap: 3rem;
-
-					padding: 0;
+					list-style-type: disc;
 					margin-left: 0.5rem;
 					margin-top: 0;
+					padding: 0;
 					width: 100%;
 				}
 
-				.textStuff {
+				.discovery-course-summary-text-stuff {
 					white-space: pre-wrap;
 				}
 			</style>
 
-			<div class="d2l-typography">
-				<div class="container">
-					<div class="breadcrumbs">
-						<d2l-link href="javascript:void(0)" on-click="_navigateToHome">[[localize('discover')]]</d2l-link>
-						<d2l-icon icon="d2l-tier1:chevron-right"></d2l-icon>
-						<d2l-link href="javascript:void(0)" on-click="_navigateToSearch">
-							<span value="[[courseCategory]]">[[courseCategory]]</span>
-						</d2l-link>
-						<d2l-icon icon="d2l-tier1:chevron-right"></d2l-icon>
-					</div>
+			<div class="discovery-course-summary-container">
+				<div class="discovery-course-summary-breadcrumbs">
+					<d2l-link href="javascript:void(0)" on-click="_navigateToHome">[[localize('discover')]]</d2l-link>
+					<d2l-icon icon="d2l-tier1:chevron-right"></d2l-icon>
+					<d2l-link href="javascript:void(0)" on-click="_navigateToSearch">
+						<span value="[[courseCategory]]">[[courseCategory]]</span>
+					</d2l-link>
+					<d2l-icon icon="d2l-tier1:chevron-right"></d2l-icon>
+				</div>
 
-					<div class="title">
-						<h1 class="d2l-heading-1">[[courseTitle]]</h1>
-					</div>
+				<div class="discovery-course-summary-title">
+					<h1 class="d2l-heading-1 discovery-course-summary-d2l-heading-1">[[courseTitle]]</h1>
+				</div>
 
-					<div class="keyOutcomes">
-						<h4 class="d2l-heading-4">[[localize('keyOutcomes')]]</h4>
-						<div class="d2l-body-compact">
-							<ul class="keyOutcomes">
-								<template is="dom-repeat" items="[[courseKeyOutcomes]]">
-									<li>[[item]]</li>
-								</template>
-							</ul>
-						</div>
+				<div class="discovery-course-summary-key-outcomes">
+					<h4 class="d2l-heading-4 discovery-course-summary-d2l-heading-4">[[localize('keyOutcomes')]]</h4>
+					<div class="d2l-body-compact">
+						<ul class="discovery-course-summary-key-outcomes">
+							<template is="dom-repeat" items="[[courseKeyOutcomes]]">
+								<li>[[item]]</li>
+							</template>
+						</ul>
 					</div>
+				</div>
 
-					<div class="description">
-						<h4 class="d2l-heading-4">[[localize('courseDescription')]]</h4>
-						<div class="d2l-body-compact textStuff">[[courseDescription]]</div>
-					</div>
+				<div class="discovery-course-summary-description">
+					<h4 class="d2l-heading-4 discovery-course-summary-d2l-heading-4">[[localize('courseDescription')]]</h4>
+					<div class="d2l-body-compact discovery-course-summary-text-stuff">[[courseDescription]]</div>
 				</div>
 			</div>
 		`;
