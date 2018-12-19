5d50923711d19315d9a8c41806820a3d7d8f81d1
Maya Jones <Maya.Jones@D2L.com>
2018-12-19 08:33:50 -0500

    Add the activity list item to the end to end test.

--- src/discovery-app.js
+++ src/discovery-app.js
@@ -2,13 +2,19 @@ import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {mixinBehaviors} from '@polymer/polymer/lib/legacy/class.js';
 import {SearchInput} from './components/search-input.js';
 import 'd2l-fetch-siren-entity-behavior/d2l-fetch-siren-entity-behavior.js';
-import { d2lfetch } from 'd2l-fetch/src/index.js';
+import 'd2l-fetch/d2l-fetch.js';
+import 'd2l-fetch-auth/d2l-fetch-auth-framed.js';
+import {D2lActivityListItem} from 'd2l-activities/components/d2l-activity-list-item/d2l-activity-list-item.js';
 
 /**
  * @customElement
  * @polymer
  */
 class DiscoveryApp extends mixinBehaviors([D2L.PolymerBehaviors.FetchSirenEntityBehavior], PolymerElement) {
+	constructor() {
+		super();
+		window.d2lfetch.use({name: 'auth', fn: window.d2lfetch.auth});
+	}
 	static get template() {
 		return html`
 			<style>
@@ -18,22 +24,33 @@ class DiscoveryApp extends mixinBehaviors([D2L.PolymerBehaviors.FetchSirenEntity
 				.discovery-flex-container {
 					display: flex;
 				}
+				.discovery-search {
+					width: 80%;
+					height: 100vh;
+				}
+				.discovery-search d2l-activity-list-item {
+					margin: 0.5rem;
+				}
 			</style>
 			<h2>Discovery</h2>
 			<div class="discovery-flex-container">
-				<img src="images/disco-self.png" />
-				<div class="search">
+				<div>
+					<img src="images/disco-self.png" />
+				</div>
+				<div class="discovery-search">
 					<search-input id="searchQuery" label="Search"></search-input>
-					<div>[[_result]]</div>
+					<template is="dom-repeat" items="[[_entitiesResult]]">
+						<d2l-activity-list-item href="#" entity=[[item]]></d2l-activity-list-item>
+					</template>
 				</div>
 			</div>
 		`;
 	}
 	static get properties() {
 		return {
-			_result: {
-				type: String,
-				value: ''
+			_entitiesResult: {
+				type: Array,
+				value: function() { return []; }
 			}
 		};
 	}
@@ -44,20 +61,20 @@ class DiscoveryApp extends mixinBehaviors([D2L.PolymerBehaviors.FetchSirenEntity
 	_onD2lInputSearchSearched(e) {
 		const searchParam = encodeURI(e.detail.value);
 		const searchUrl = `https://us-east-1.discovery.bff.dev.brightspace.com/search?q=${searchParam}`;
-		this._fetchEntityWithToken(searchUrl, this.getToken.bind(this))
+		this._fetchEntity(searchUrl)
 			.then(this._handleSearchResponse.bind(this));
 	}
 
 	_handleSearchResponse(sirenEntity) {
-		this._result = sirenEntity;
-		console.log(sirenEntity);
+		const entities = sirenEntity.getSubEntitiesByRel('https://discovery.brightspace.com');
+		this._entitiesResult = entities;
 	}
 
 	getToken() {
 		const scope = ['*:*:*'];
 		const client = window.D2L.frau.client;
 		return client.request('frau-jwt-new-jwt', scope);
-	  }
+	}
 }
 
 window.customElements.define('discovery-app', DiscoveryApp);
