2532d8c7418997270285c7c5213ea35e68ac512b
Maya Jones <Maya.Jones@D2L.com>
2019-01-10 14:34:53 -0500

    Add skeleton test and linting

--- scripts/getHostname.js
+++ scripts/getHostname.js
@@ -1,20 +1,5 @@
-#! /usr/bin/env node
-const os = require('os');
+const os = require( 'os' );
 
 (() => {
-	const networkInterfaces = os.networkInterfaces();
-
-	for (var key in networkInterfaces) {
-		if (networkInterfaces.hasOwnProperty(key)) {
-			const addresses = networkInterfaces[key];
-			const res = addresses.filter((addressObj) => {
-				return addressObj.family === 'IPv4' && addressObj.internal === false;
-			});
-
-			if (res && res.length > 0 && res[0] && res[0].address) {
-				console.log(res[0].address); // eslint-disable-line no-console
-				return;
-			}
-		}
-	}
+  return console.log(os.hostname().toLowerCase());
 })();
--- src/tools/filter-helpers.js
+++ src/tools/filter-helpers.js
@@ -1,14 +1,14 @@
 'use strict';
 
-function _camelize(text) {
-  // String.prototype.toCamelCase = function() {
-  //   return this
-  //     .replace(/\s(.)/g, function($1) { return $1.toUpperCase(); })
-  //     .replace(/\s/g, '')
-  //     // .replace(/^(.)/, function($1) { return $1.toLowerCase(); });
-  // }
-  return text
-    .replace(/\s(.)/g, function($1) { return $1.toUpperCase(); })
-    .replace(/\s/g, '')
-    .replace(/^(.)/, function($1) { return $1.toLowerCase(); });
+function _camelize(text) { // eslint-disable-line
+	// String.prototype.toCamelCase = function() {
+	//   return this
+	//     .replace(/\s(.)/g, function($1) { return $1.toUpperCase(); })
+	//     .replace(/\s/g, '')
+	//     // .replace(/^(.)/, function($1) { return $1.toLowerCase(); });
+	// }
+	return text
+		.replace(/\s(.)/g, function($1) { return $1.toUpperCase(); })
+		.replace(/\s/g, '')
+		.replace(/^(.)/, function($1) { return $1.toLowerCase(); });
 }
