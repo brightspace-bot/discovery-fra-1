c70e2c49e9631d72123bb4cc5fa3b38cd6837628
Maya Jones <Maya.Jones@D2L.com>
2019-03-01 13:13:25 -0500

    Build language files

---
+++ src/build/lang/pt.js
@@ -0,0 +1,43 @@
+/* eslint-disable quotes */
+window.D2L = window.D2L || {};
+window.D2L.Discovery = window.D2L.Discovery || {};
+/**
+* This file is auto-generated. Do not modify.
+* @polymerBehavior D2L.Discovery.LangPt
+*/
+window.D2L.Discovery.LangPt = {
+	"accessMaterials": "Materiais de acesso",
+	"addToMyList": "Adicionar á minha lista",
+	"addedToMyLearningHeader": "Adicionado com êxito à Minha aprendizagem",
+	"addedToYourList": "Adicionado à sua lista",
+	"backToDiscovery": "Voltar à descoberta",
+	"continueBrowsing": "Continuar procurando",
+	"courseCode": "Código do Curso",
+	"courseDescription": "Descrição do Curso",
+	"courseInfo": "Informações do curso",
+	"discover": "Descobrir",
+	"discovery": "Descoberta",
+	"durationMinutes": "{minutes} minutos",
+	"endDate": "Data final",
+	"enrollInCourse": "Inscrever-se no curso",
+	"enrollmentMessage.fail": "Unable to enroll at this time, please try again later.",
+	"enrollmentMessage.success": "You can find the course in the My Courses widget.",
+	"firstPublished": "Primeira publicação",
+	"lastUpdatedDate": "Última atualização {date}",
+	"onMyList": "Em minha lista",
+	"openCourse": "Open Course",
+	"pageNext": "Até a próxima página",
+	"pagePrevious": "Até a página anterior",
+	"pageSelection": "Na página {pageCurrent} de {pageTotal}. Insira um número de página para ir até ela",
+	"pageSelectionInvalid": "O valor deve estar entre 1 e {pageTotal}",
+	"resultsFor": "{amount} resultados para \"{searchQuery}\"",
+	"search": "Pesquisar",
+	"search.placeholder": "Pesquisar...",
+	"searchKeywords": "Pesquisar palavras-chave",
+	"searchResultCount": "{searchResultRange} de {searchResultsTotal} para \"{searchQuery}\"",
+	"startDate": "Data de início",
+	"startLearning": "Iniciar aprendizagem",
+	"unenroll": "Cancelar Inscrição",
+	"viewMyList": "Exibir minha lista",
+	"welcomeToTheCourse": "Bem-vindo(a) ao curso!"
+};
---
+++ src/build/lang/zh-tw.js
@@ -0,0 +1,43 @@
+/* eslint-disable quotes */
+window.D2L = window.D2L || {};
+window.D2L.Discovery = window.D2L.Discovery || {};
+/**
+* This file is auto-generated. Do not modify.
+* @polymerBehavior D2L.Discovery.LangZhtw
+*/
+window.D2L.Discovery.LangZhtw = {
+	"accessMaterials": "存取資料",
+	"addToMyList": "新增至我的清單",
+	"addedToMyLearningHeader": "已成功新增至「我的學習」(My Learning)",
+	"addedToYourList": "已新增至您的清單",
+	"backToDiscovery": "返回探索",
+	"continueBrowsing": "繼續瀏覽",
+	"courseCode": "課程代碼",
+	"courseDescription": "課程說明",
+	"courseInfo": "課程資訊",
+	"discover": "探索",
+	"discovery": "探索",
+	"durationMinutes": "{minutes} 分鐘",
+	"endDate": "結束日期",
+	"enrollInCourse": "註冊課程",
+	"enrollmentMessage.fail": "Unable to enroll at this time, please try again later.",
+	"enrollmentMessage.success": "You can find the course in the My Courses widget.",
+	"firstPublished": "首次發佈",
+	"lastUpdatedDate": "上次更新時間：{date}",
+	"onMyList": "在我的清單上",
+	"openCourse": "Open Course",
+	"pageNext": "前往下一頁",
+	"pagePrevious": "前往上一頁",
+	"pageSelection": "在第 {pageCurrent} 頁，共 {pageTotal} 頁。請輸入頁面號碼以前往該頁面",
+	"pageSelectionInvalid": "值必須介於 1 與 {pageTotal} 之間",
+	"resultsFor": "{amount} 個結果符合 「{searchQuery}」",
+	"search": "搜尋",
+	"search.placeholder": "搜尋...",
+	"searchKeywords": "搜尋關鍵字",
+	"searchResultCount": "符合「{searchQuery}」的結果：{searchResultRange}/{searchResultsTotal}",
+	"startDate": "開始日期",
+	"startLearning": "開始學習",
+	"unenroll": "取消註冊",
+	"viewMyList": "檢視我的清單",
+	"welcomeToTheCourse": "歡迎參加本課程！"
+};
