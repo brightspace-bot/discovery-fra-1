e25ee767bd2271daa918cc47297735e3dd9c1db0
Quan Hui Wang <quanhui.wang@d2l.com>
2019-03-25 11:40:44 -0400

    Add a mailto link in the discovery-footer.

--- src/components/discovery-footer.js
+++ src/components/discovery-footer.js
@@ -1,16 +1,29 @@
 'use strict';
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import { LocalizeMixin } from '../mixins/localize-mixin.js';
+import 'd2l-link/d2l-link.js';
 
-class DiscoveryFooter extends PolymerElement {
+class DiscoveryFooter extends LocalizeMixin(PolymerElement) {
 	static get template() {
 		return html`
 			<style>
 				:host {
-					display: inline-block;
+					display: block;
 					height: 150px;
 				}
+				.discovery-footer-feedback-email {
+					padding-top: 15px;
+					padding-bottom: 10px;
+					text-align: right;
+				}
 			</style>
-			<footer></footer>
+			<footer>
+				<div class="discovery-footer-feedback-email">
+					<d2l-link href="mailto:self-enroll-pilot@d2lmail.onmicrosoft.com">
+						[[localize('emailFeedback', 'email', 'self-enroll-pilot@d2lmail.onmicrosoft.com')]]
+					</d2l-link>
+				</div>
+			</footer>
 		`;
 	}
 	static get properties() {
