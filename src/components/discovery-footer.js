8e5660dd8672056e643c2d364e8c751e11d63d96
Quan Hui Wang <quanhui.wang@d2l.com>
2019-04-24 15:32:53 -0400

    Remove feedback from the footer. The footer should still be 150px height at the end of all pages.

--- src/components/discovery-footer.js
+++ src/components/discovery-footer.js
@@ -1,9 +1,7 @@
 'use strict';
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
-import { LocalizeMixin } from '../mixins/localize-mixin.js';
-import 'd2l-link/d2l-link.js';
 
-class DiscoveryFooter extends LocalizeMixin(PolymerElement) {
+class DiscoveryFooter extends PolymerElement {
 	static get template() {
 		return html`
 			<style>
@@ -11,19 +9,8 @@ class DiscoveryFooter extends LocalizeMixin(PolymerElement) {
 					display: block;
 					height: 150px;
 				}
-				.discovery-footer-feedback-email {
-					padding-top: 15px;
-					padding-bottom: 10px;
-					text-align: right;
-				}
 			</style>
-			<footer>
-				<div class="discovery-footer-feedback-email">
-					<d2l-link href="mailto:self-enroll-pilot@d2lmail.onmicrosoft.com">
-						[[localize('emailFeedback', 'email', 'self-enroll-pilot@d2lmail.onmicrosoft.com')]]
-					</d2l-link>
-				</div>
-			</footer>
+			<footer></footer>
 		`;
 	}
 	static get properties() {
