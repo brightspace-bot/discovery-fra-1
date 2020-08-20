91437e46af3aafa51377520bfcc007674f698f2e
Edward Gao <Edward.Gao@D2L.com>
2020-08-20 14:58:28 -0400

    DE40008-add paddings to selection count

--- src/components/discover-settings-promoted-content.js
+++ src/components/discover-settings-promoted-content.js
@@ -110,10 +110,11 @@ class DiscoverSettingsPromotedContent extends DiscoverSettingsMixin(RouteLocatio
 			.discover-featured-selected-nav {
 				display:flex;
 				justify-content: flex-end;
-				width: 50%
+				width: 50%;
 				height: fit-content;
 			}
 			.discover-featured-selected-nav-count {
+				margin-left: 1rem;
 				margin-right: .5rem;
 			}
 			.discover-featured-selected-nav-clear {
